d5c29a62c69e0cdb15dc68c863e0ba97b72aa022
Dylan Sturgeon <dylansturg@gmail.com>
2015-06-14 17:30:04 -0400

    name changes

--- EPGPWiper/EPGPWiper.lua
+++ EPGPWiper/EPGPWiper.lua
@@ -4,7 +4,7 @@
 -- Command(s):
 --    /wipe - reset EPGP data to 0,0 (officer note) for all guild members
 
-SLASH_EPGPWIPER_WIPE = '/epgpwipe';
+SLASH_EPGPWIPER1 = '/epgpwipe';
 
 local function handler(msg, editbox)
   print("You invoked my slash command.  Awesome job.");
