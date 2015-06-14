d01ce4c99862e73c94af34f26cb9aab8169f56b5
Dylan Sturgeon <dylansturg@gmail.com>
2015-06-14 17:25:34 -0400

    adding slash command to epgp wiper addon

--- EPGPWiper/EPGPWiper.lua
+++ EPGPWiper/EPGPWiper.lua
@@ -1,3 +1,17 @@
+
+-- Slash command(s)
+-- Prefix: EPGPWIPER
+-- Command(s):
+--    /wipe - reset EPGP data to 0,0 (officer note) for all guild members
+
+SLASH_EPGPWIPER_WIPE = '/epgpwipe';
+
+local function handler(msg, editbox)
+  print("You invoked my slash command.  Awesome job.");
+end
+
+SlashCmdList["EPGPWIPER"] = handler;
+
 function AlertClicked()
   print("You clicked the button!");
 end
